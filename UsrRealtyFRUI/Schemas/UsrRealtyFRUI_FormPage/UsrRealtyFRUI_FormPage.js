48686d83600cc4d0db17e043f2e78bd00de2f75d
Madhu Sanjeeva <madhusanjeeva91@gmail.com>
2023-09-25 16:54:43 +0530

    Reality Freedom UI form and added fields

--- UsrRealtyFRUI/Schemas/UsrRealtyFRUI_FormPage/UsrRealtyFRUI_FormPage.js
+++ UsrRealtyFRUI/Schemas/UsrRealtyFRUI_FormPage/UsrRealtyFRUI_FormPage.js
@@ -2,6 +2,87 @@ define("UsrRealtyFRUI_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 	return {
 		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
 			{
+				"operation": "remove",
+				"name": "CardButtonToggleGroup"
+			},
+			{
+				"operation": "merge",
+				"name": "Tabs",
+				"values": {
+					"styleType": "default",
+					"mode": "tab",
+					"bodyBackgroundColor": "primary-contrast-500",
+					"selectedTabTitleColor": "auto",
+					"tabTitleColor": "auto",
+					"underlineSelectedTabColor": "auto",
+					"headerBackgroundColor": "auto",
+					"layoutConfig": {}
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "GeneralInfoTabContainer",
+				"values": {
+					"gap": {
+						"columnGap": "large",
+						"rowGap": "none"
+					},
+					"visible": true,
+					"color": "transparent",
+					"borderRadius": "none",
+					"padding": {
+						"top": "none",
+						"right": "none",
+						"bottom": "none",
+						"left": "none"
+					}
+				}
+			},
+			{
+				"operation": "remove",
+				"name": "CardToggleTabPanel"
+			},
+			{
+				"operation": "remove",
+				"name": "FeedTabContainer"
+			},
+			{
+				"operation": "remove",
+				"name": "Feed"
+			},
+			{
+				"operation": "remove",
+				"name": "FeedTabContainerHeaderContainer"
+			},
+			{
+				"operation": "remove",
+				"name": "FeedTabContainerHeaderLabel"
+			},
+			{
+				"operation": "remove",
+				"name": "AttachmentsTabContainer"
+			},
+			{
+				"operation": "remove",
+				"name": "AttachmentList"
+			},
+			{
+				"operation": "remove",
+				"name": "AttachmentsTabContainerHeaderContainer"
+			},
+			{
+				"operation": "remove",
+				"name": "AttachmentsTabContainerHeaderLabel"
+			},
+			{
+				"operation": "remove",
+				"name": "AttachmentAddButton"
+			},
+			{
+				"operation": "remove",
+				"name": "AttachmentRefreshButton"
+			},
+			{
 				"operation": "insert",
 				"name": "UsrName",
 				"values": {
@@ -14,69 +95,242 @@ define("UsrRealtyFRUI_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 					"type": "crt.Input",
 					"label": "$Resources.Strings.UsrName",
 					"control": "$UsrName",
-					"labelPosition": "auto"
+					"labelPosition": "auto",
+					"multiline": false
 				},
 				"parentName": "SideAreaProfileContainer",
 				"propertyName": "items",
 				"index": 0
 			},
 			{
-				"operation": "merge",
-				"name": "AttachmentList",
+				"operation": "insert",
+				"name": "UsrPriceUSD",
 				"values": {
-					"type": "crt.FileList",
-					"masterRecordColumnValue": "$Id",
-					"recordColumnName": "RecordId",
 					"layoutConfig": {
-						"colSpan": 2,
+						"column": 1,
+						"row": 2,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.NumberAttribute_3sftxcy",
+					"labelPosition": "auto",
+					"control": "$NumberAttribute_3sftxcy"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "UsrArea",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.NumberAttribute_xtoxqb7",
+					"labelPosition": "auto",
+					"control": "$NumberAttribute_xtoxqb7"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "UsrNumber",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 4,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.StringAttribute_m647n1l",
+					"labelPosition": "auto",
+					"control": "$StringAttribute_m647n1l",
+					"multiline": false
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "UsrType",
+				"values": {
+					"layoutConfig": {
 						"column": 1,
 						"row": 1,
-						"rowSpan": 6
+						"colSpan": 1,
+						"rowSpan": 1
 					},
-					"items": "$AttachmentList",
-					"primaryColumnName": "AttachmentListDS_Id",
-					"columns": [
-						{
-							"id": "02488a94-508e-4dc2-bec4-f785c384b5ad",
-							"code": "AttachmentListDS_Name",
-							"caption": "#ResourceString(AttachmentListDS_Name)#",
-							"dataValueType": 28,
-							"width": 200
-						}
-					],
-					"viewType": "gallery",
-					"tileSize": "small"
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_f7juhgq",
+					"labelPosition": "auto",
+					"control": "$LookupAttribute_f7juhgq",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": []
 				},
-				"parentName": "AttachmentsTabContainer",
+				"parentName": "GeneralInfoTabContainer",
 				"propertyName": "items",
 				"index": 0
 			},
 			{
-				"operation": "merge",
-				"name": "Feed",
+				"operation": "insert",
+				"name": "addRecord_je8g7ef",
 				"values": {
-					"type": "crt.Feed",
-					"feedType": "Record",
-					"primaryColumnValue": "$Id",
-					"cardState": "$CardState",
-					"dataSourceName": "PDS",
-					"entitySchemaName": "UsrRealtyFRUI"
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_je8g7ef_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
 				},
-				"parentName": "FeedTabContainer",
+				"parentName": "UsrType",
+				"propertyName": "listActions",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "UsrOfferType",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_4mectv0",
+					"labelPosition": "auto",
+					"control": "$LookupAttribute_4mectv0",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": []
+				},
+				"parentName": "GeneralInfoTabContainer",
 				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_186no3u",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_186no3u_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "UsrOfferType",
+				"propertyName": "listActions",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "UsrComment",
+				"values": {
+					"type": "crt.Input",
+					"label": "$Resources.Strings.StringAttribute_y315pxq",
+					"labelPosition": "auto",
+					"control": "$StringAttribute_y315pxq",
+					"multiline": false
+				},
+				"parentName": "GeneralInfoTab",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "UsrManager",
+				"values": {
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_pc893ca",
+					"labelPosition": "auto",
+					"control": "$LookupAttribute_pc893ca",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": []
+				},
+				"parentName": "GeneralInfoTab",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_sy88f3r",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_sy88f3r_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "UsrManager",
+				"propertyName": "listActions",
 				"index": 0
 			}
 		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
 		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
 			"attributes": {
+				"Id": {
+					"modelConfig": {
+						"path": "PDS.Id"
+					}
+				},
 				"UsrName": {
 					"modelConfig": {
 						"path": "PDS.UsrName"
 					}
 				},
-				"Id": {
+				"NumberAttribute_3sftxcy": {
 					"modelConfig": {
-						"path": "PDS.Id"
+						"path": "PDS.UsrPriceUSD"
+					}
+				},
+				"NumberAttribute_xtoxqb7": {
+					"modelConfig": {
+						"path": "PDS.UsrArea"
+					}
+				},
+				"LookupAttribute_f7juhgq": {
+					"modelConfig": {
+						"path": "PDS.UsrType"
+					}
+				},
+				"LookupAttribute_4mectv0": {
+					"modelConfig": {
+						"path": "PDS.UsrOfferType"
+					}
+				},
+				"LookupAttribute_pc893ca": {
+					"modelConfig": {
+						"path": "PDS.UsrManager"
+					}
+				},
+				"StringAttribute_y315pxq": {
+					"modelConfig": {
+						"path": "PDS.UsrComment"
+					}
+				},
+				"StringAttribute_m647n1l": {
+					"modelConfig": {
+						"path": "PDS.UsrNumber"
 					}
 				}
 			}
@@ -89,18 +343,6 @@ define("UsrRealtyFRUI_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**
 						"entitySchemaName": "UsrRealtyFRUI"
 					},
 					"scope": "page"
-				},
-				"AttachmentListDS": {
-					"type": "crt.EntityDataSource",
-					"scope": "viewElement",
-					"config": {
-						"entitySchemaName": "SysFile",
-						"attributes": {
-							"Name": {
-								"path": "Name"
-							}
-						}
-					}
 				}
 			},
 			"primaryDataSourceName": "PDS"
